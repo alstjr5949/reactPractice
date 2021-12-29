24d88d42f7c0e11ba365d2d43307f8e181be742d
alstjr5949 <alstjr5949@gmail.com>
2021-12-30 00:02:30 +0900

    useEffect chapter 6 finish

--- src/App.js
+++ src/App.js
@@ -1,10 +1,29 @@
-import Button from "./Button";
+import { useState, useEffect } from "react";
+import styles from "./Button.module.css";
 
 function App() {
+  const [counter, setValue] = useState(0);
+  const [keyword, setKeyword] = useState("");
+  const onClick = () => setValue((prev) => prev + 1);
+  const onChange = (event) => setKeyword(event.target.value);
+  useEffect(() => {
+    console.log("CALL THE API....");
+  }, []);
+  useEffect(() => {
+    console.log("SEARCH FOR", keyword);
+  }, [keyword]);
   return (
     <div>
-      <h1>Welcome Back!!!</h1>
-      <Button text={"Continue"} />
+      <input
+        value={keyword}
+        onChange={onChange}
+        type="text"
+        placeholder="Search here..."
+      />
+      <h1>{counter}</h1>
+      <button className={styles.btn} onClick={onClick}>
+        Click me
+      </button>
     </div>
   );
 }
