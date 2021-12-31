00c5008a4509575e85ce115d1614735f23d96395
alstjr5949 <alstjr5949@gmail.com>
2021-12-31 20:53:02 +0900

    movie app concept

--- src/App.js
+++ src/App.js
@@ -1,52 +1,21 @@
 import { useState, useEffect } from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Home from "./routes/Home";
+import Detail from "./routes/Detail";
 import "./App.css";
 
 function App() {
-  const [loading, setLoading] = useState(true);
-  const [coins, setCoins] = useState([]);
-  const [seed, setSeed] = useState(0);
-  const onChange = (event) => {
-    setSeed(event.target.value);
-  };
-  useEffect(() => {
-    fetch(
-      "https://api.coingecko.com/api/v3/coins/markets?vs_currency=krw&order=market_cap_desc&per_page=100&page=1&sparkline=false"
-    )
-      .then((response) => response.json())
-      .then((json) => {
-        setCoins(json);
-        setLoading(false);
-      });
-  }, []);
   return (
-    <div className="coinWrapper">
-      <h1>The Coins! ({coins.length})</h1>
-      <label htmlFor="seedMoney">Your Seed Money(Won)</label>
-      <input
-        onChange={onChange}
-        value={seed}
-        id="seedMoney"
-        type="number"
-        placeholder="Your seed money(원)"
-      />
-      {loading ? (
-        <strong>Loading...</strong>
-      ) : (
-        <div>
-          {coins.map((coin) => (
-            <div className="coinDiv">
-              <img src={coin.image} />
-              <span>
-                {coin.name} ({coin.symbol}) : {coin.current_price} 원
-              </span>
-              <span>
-                구매가능 개수 : {(seed / coin.current_price).toFixed(4)} 개
-              </span>
-            </div>
-          ))}
-        </div>
-      )}
-    </div>
+    <Router>
+      <Switch>
+        <Route path="/movie/:id">
+          <Detail />
+        </Route>
+        <Route path="/">
+          <Home />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
---
+++ src/routes/Detail.js
@@ -0,0 +1,18 @@
+import { useParams } from "react-router-dom";
+import { useEffect } from "react/cjs/react.development";
+
+function Detail() {
+  const { id } = useParams();
+  const getMovie = async () => {
+    const json = await (
+      await fetch(`https://yts.mx/api/v2/list_movies.json?id=${id}`)
+    ).json();
+    console.log(json);
+  };
+  useEffect(() => {
+    getMovie();
+  }, []);
+  return <h1>Detail</h1>;
+}
+
+export default Detail;
