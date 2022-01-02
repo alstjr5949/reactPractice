4d91af9fce1ead8152fa60e3c5c096a56e1a1517
alstjr5949 <alstjr5949@gmail.com>
2022-01-02 21:40:27 +0900

    movie app before css

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,18 +1,46 @@
 import { useParams } from "react-router-dom";
-import { useEffect } from "react/cjs/react.development";
+import { useEffect, useState } from "react/cjs/react.development";
 
 function Detail() {
   const { id } = useParams();
+  const [loading, setLoading] = useState(true);
+  const [movie, setMovie] = useState([]);
   const getMovie = async () => {
     const json = await (
-      await fetch(`https://yts.mx/api/v2/list_movies.json?id=${id}`)
+      await fetch(`https://yts.mx/api/v2/movie_details.json?movie_id=${id}`)
     ).json();
-    console.log(json);
+    setMovie(json.data.movie);
+    setLoading(false);
   };
   useEffect(() => {
     getMovie();
   }, []);
-  return <h1>Detail</h1>;
+  return (
+    <div>
+      {loading ? (
+        <h1>Loading ...</h1>
+      ) : (
+        <div>
+          <div>
+            <h1>Movie Details</h1>
+          </div>
+          <div>
+            <div>
+              <img src={movie.medium_cover_image} />
+            </div>
+            <div>
+              <div>
+                <h1>{movie.title}</h1>
+                <h6>{movie.year}</h6>
+                <h6>{movie.rating}</h6>
+                <p>{movie.description_intro}</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      )}
+    </div>
+  );
 }
 
 export default Detail;
